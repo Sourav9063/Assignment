a8eed73dc91cd1b89f9a27f15ef01cc9db0540e4
Sourav9063 <53114581+Sourav9063@users.noreply.github.com>
2020-02-28 14:00:44 +0600

    fibonacci method

--- src/Fibonacci/FibonacciMain.java
+++ src/Fibonacci/FibonacciMain.java
@@ -12,8 +12,27 @@ package Fibonacci;
 public class FibonacciMain {
     
     
-    public static void main(String[] args) {
-        System.out.println("Hello");
-    }
+//    public static void main(String[] args) {
+//        System.out.println("Hello");
+//    }
+    
+    
+    public int fibonacci(int n){
+    if(n==0)
+        return 0;
+    else if(n==1)
+      return 1;
+   else
+      return fibonacci(n-1)+fibonacci(n-2);
+}
+
+    
+    
+    
+    
+    
+    
+    
+    
     
 }
