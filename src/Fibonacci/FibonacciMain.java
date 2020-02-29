03c800c332700be9f1f5f3cdef051e8032f923fd
Sourav9063 <53114581+Sourav9063@users.noreply.github.com>
2020-02-29 09:19:40 +0600

    Complete fibonacci

--- src/Fibonacci/FibonacciMain.java
+++ src/Fibonacci/FibonacciMain.java
@@ -17,16 +17,14 @@ public class FibonacciMain {
 //    }
     
     
-    public int fibonacci(int n){
-    if(n==0)
-        return 0;
-    else if(n==1)
-      return 1;
-   else
-      return fibonacci(n-1)+fibonacci(n-2);
-}
 
-    
+
+    public static void main(String[] args) {
+        Fibonacci fib=new Fibonacci();
+        int n=11;
+        int ans=fib.fibonacci(n);
+        System.out.println(ans);
+    }
     
     
     
